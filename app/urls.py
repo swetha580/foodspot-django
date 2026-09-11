1803bede0e906c5b365d1ba9cc91ccb6da2c0aae
Swetha <swetha@testpress.in>
2026-09-11 15:11:24 +0530

    Add user registration (signup) form, view, and template

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.views import home
+from app.views import home, signup
 
 urlpatterns = [
     path('home/', home, name='home'),
+    path('signup/', signup, name='signup'),
 ]
