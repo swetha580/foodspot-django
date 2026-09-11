a5cccfa7ac8cb330e8a05681d2dc45c8ed2dab70
Swetha <swetha@testpress.in>
2026-09-11 17:26:18 +0530

    Add restaurant detail view with menu, cuisines, and photos

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from app.views import home, signup, restaurant_list
+from app.views import home, signup, restaurant_list, restaurant_detail
 from app.forms import StyledAuthenticationForm, StyledPasswordResetForm, StyledSetPasswordForm
 
 urlpatterns = [
@@ -28,4 +28,5 @@ urlpatterns = [
         template_name='app/password_reset_complete.html'
     ), name='password_reset_complete'),
     path('restaurants/', restaurant_list, name='restaurant_list'),
+    path('restaurants/<int:pk>/', restaurant_detail, name='restaurant_detail'),
 ]
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,3 +1,3 @@
 from .home_views import home
 from .auth_views import signup
-from .restaurant_views import restaurant_list
+from .restaurant_views import restaurant_list, restaurant_detail
