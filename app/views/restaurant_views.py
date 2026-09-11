88d3419e2818a424e62e7a333c81e6fa39261332
Swetha <swetha@testpress.in>
2026-09-11 16:57:25 +0530

    Add paginated restaurant list view

---
+++ app/views/restaurant_views.py
@@ -0,0 +1,11 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from app.models import Restaurant
+
+
+def restaurant_list(request):
+    restaurants = Restaurant.objects.all().order_by('name')
+    paginator = Paginator(restaurants, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'app/restaurant_list.html', {'page_obj': page_obj})
