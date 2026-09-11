ef244359a7353437d6d248627cf548d844e83121
Swetha <swetha@testpress.in>
2026-09-11 18:02:08 +0530

    Add sorting by rating and cost, and fix pagination to preserve filters/sort

--- app/views/restaurant_views.py
+++ app/views/restaurant_views.py
@@ -3,6 +3,14 @@ from app.models import Restaurant, Cuisine
 from app.filters import RestaurantFilter
 
 
+SORT_OPTIONS = {
+    'rating_high': '-average_rating',
+    'rating_low': 'average_rating',
+    'cost_high': '-cost_for_two',
+    'cost_low': 'cost_for_two',
+}
+
+
 class RestaurantListView(ListView):
     model = Restaurant
     template_name = 'app/restaurant_list.html'
@@ -11,12 +19,24 @@ class RestaurantListView(ListView):
     def get_queryset(self):
         queryset = Restaurant.objects.all().order_by('name')
         self.filterset = RestaurantFilter(self.request.GET, queryset=queryset)
-        return self.filterset.qs.distinct()
+        queryset = self.filterset.qs.distinct()
+
+        sort = self.request.GET.get('sort')
+        if sort in SORT_OPTIONS:
+            queryset = queryset.order_by(SORT_OPTIONS[sort])
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filterset'] = self.filterset
         context['cuisines'] = Cuisine.objects.all().order_by('name')
+        context['selected_sort'] = self.request.GET.get('sort', '')
+
+        query_params = self.request.GET.copy()
+        query_params.pop('page', None)
+        context['query_string'] = query_params.urlencode()
+
         return context
 
 
