c9f8b5038f2207236bf5c9061b008db080e2f114
Swetha <swetha@testpress.in>
2026-09-16 14:34:25 +0530

    refactor: let RestaurantFilter own filter state, remove redundant context variables

--- app/filters.py
+++ app/filters.py
@@ -1,19 +1,14 @@
 import django_filters
-from app.models import Restaurant
-
-VEG_TYPE_CHOICES = Restaurant._meta.get_field('veg_type').choices
-IS_OPEN_CHOICES = (
-    ('open', 'Open'),
-    ('closed', 'Closed'),
-)
+from app.models import Restaurant, Cuisine
 
 
 class RestaurantFilter(django_filters.FilterSet):
-    city = django_filters.CharFilter(field_name='city', lookup_expr='iexact')
-    cuisine = django_filters.NumberFilter(field_name='cuisines__id')
-    veg_type = django_filters.ChoiceFilter(choices=VEG_TYPE_CHOICES)
+    city = django_filters.ChoiceFilter(choices=[])
+    cuisine = django_filters.ModelChoiceFilter(queryset=Cuisine.objects.all().order_by('name'))
+    veg_type = django_filters.ChoiceFilter(choices=Restaurant._meta.get_field('veg_type').choices)
     is_open = django_filters.ChoiceFilter(
-        choices=IS_OPEN_CHOICES, method='filter_is_open'
+        choices=(('open', 'Open'), ('closed', 'Closed')),
+        method='filter_is_open'
     )
     min_cost = django_filters.NumberFilter(field_name='cost_for_two', lookup_expr='gte')
     max_cost = django_filters.NumberFilter(field_name='cost_for_two', lookup_expr='lte')
@@ -21,6 +16,11 @@ class RestaurantFilter(django_filters.FilterSet):
     class Meta:
         model = Restaurant
         fields = ['city', 'cuisine', 'veg_type', 'is_open', 'min_cost', 'max_cost']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        cities = Restaurant.objects.values_list('city', flat=True).distinct().order_by('city')
+        self.filters['city'].extra['choices'] = [(city, city) for city in cities]
 
     def filter_is_open(self, queryset, name, value):
         if value == 'open':
--- app/views/restaurant_views.py
+++ app/views/restaurant_views.py
@@ -15,15 +15,10 @@ class RestaurantListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['cities'] = Restaurant.objects.values_list('city', flat=True).distinct().order_by('city')
+        context['filterset'] = self.filterset
         context['cuisines'] = Cuisine.objects.all().order_by('name')
-        context['selected_city'] = self.request.GET.get('city', '')
-        context['selected_cuisine'] = self.request.GET.get('cuisine', '')
-        context['selected_veg_type'] = self.request.GET.get('veg_type', '')
-        context['selected_is_open'] = self.request.GET.get('is_open', '')
-        context['min_cost'] = self.request.GET.get('min_cost', '')
-        context['max_cost'] = self.request.GET.get('max_cost', '')
         return context
+
 
 class RestaurantDetailView(DetailView):
     model = Restaurant
